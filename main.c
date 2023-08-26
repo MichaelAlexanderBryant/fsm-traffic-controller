4728142a122a8e890a2be2992ce5837fbacd5122
MichaelAlexanderBryant <m.alexander.bryant@gmail.com>
2023-08-26 16:29:30 -0700

    Set pins in port A based on port C

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include "stm32f4xx.h"
 
-struct State {
-	uint32_t lights_output;
+struct state {
+	uint32_t set_lights; // 6-bit output where left three are for North and right three are for East (green, yellow, red)
 	uint32_t time_delay;
 	uint32_t next_state[4];
 };
 
-typedef const struct State State_t;
+typedef const struct state state_t;
 
 // states
 #define GO_NORTH 0
@@ -14,27 +14,32 @@ typedef const struct State State_t;
 #define	GO_EAST 2
 #define WAIT_EAST 3
 
-// finite state machine {6-bit output, time delay, state transitions}
-State_t FSM[4]={
+// finite-state machine {6-bit output, time delay, state transitions}
+state_t FSM[4]={
  {0x21,3000,{GO_NORTH,WAIT_NORTH,GO_NORTH,WAIT_NORTH}},
  {0x22, 500,{GO_EAST,GO_EAST,GO_EAST,GO_EAST}},
  {0x0C,3000,{GO_EAST,GO_EAST,WAIT_EAST,WAIT_EAST}},
  {0x14, 500,{GO_NORTH,GO_NORTH,GO_NORTH,GO_NORTH}}
 };
 
-uint32_t current_state; // index to the current state
+uint32_t current_state; // index the current state
 uint32_t input; // 2-bit sensor reading
 
 int main(void) {
 	// initialize phase-lock loop (PLL) at 80MHz
 	// initialize SysTick
-	// initialize port clocks
-	// initialize inputs and outputs	
+	RCC->AHB1ENR |= 1; // enable GPIOA clock (output)
+	RCC->AHB1ENR |= 4; // enable GPIOC clock (input)
+
+	GPIOA->MODER &= 0x00000FFF; // PA0-PA5 (output)
+	GPIOA->MODER |= 0x00000555; // 010101010101
+	GPIOC->MODER &= 0x0000000F; // PC0-PC1 (input)
+	
 	current_state = GO_NORTH; // initial state
 	while(1){
-		// output LIGHT = FSM[current_state].lights_output
+		GPIOA->ODR = FSM[current_state].set_lights;
 		// wait FSM[current_state].time_delay
-		// read SENSORS Input = SENSOR
+		input = GPIOC->IDR;
 		current_state = FSM[current_state].next_state[input];
 	}
 }
