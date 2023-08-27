19b204b4231923fe02cb99bb74e7c9c7409f7a2e
MichaelAlexanderBryant <m.alexander.bryant@gmail.com>
2023-08-26 19:05:21 -0700

    Initialize phase-lock loop

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 // finite-state machine {6-bit output, time delay, state transitions}
 struct state {
-	uint32_t set_lights;
+	uint32_t set_output;
 	uint32_t time_delay;
 	uint32_t next_state[4];
 };
@@ -22,11 +22,14 @@ state_t FSM[4]={
  {0x14, 500,{GO_Y,GO_Y,GO_Y,GO_Y}}
 };
 
+void ppl_init(void);
+void systick_init(void);
+
 uint32_t current_state; // index the current state
 uint32_t input; // 2-bit sensor reading
 
 int main(void) {
-	// initialize phase-lock loop (PLL) at 80MHz
+	ppl_init(); // initialize phase-lock loop (PPL) at 80MHz
 	// initialize SysTick
 	RCC->AHB1ENR |= 1; // enable GPIOA clock (output)
 	RCC->AHB1ENR |= 4; // enable GPIOC clock (input)
@@ -37,9 +40,54 @@ int main(void) {
 	
 	current_state = GO_Y; // initial state
 	while(1){
-		GPIOA->ODR = FSM[current_state].set_lights;
+		GPIOA->ODR = FSM[current_state].set_output;
 		// wait FSM[current_state].time_delay
 		input = GPIOC->IDR;
 		current_state = FSM[current_state].next_state[input];
 	}
+}
+
+void ppl_init(void){
+	// PLLP output: 80MHz
+	// PLLP input: 160MHz (Target*2)
+	// PLLM output: 1MHz
+	// PLLM input (Reference): 8MHz
+	// PLLN output: 160MHz (PLLM output*PLLP input)
+	// PLLM=8, PLLN=160, PLLP=2
+	
+	RCC->CR |= (1<<16); // enable HSE clock
+	while(!(RCC->CR & (1<<17))); // wait for HSE
+	RCC->CR &= ~(1<<24); // disable PLL
+
+	// configure PLLM
+	RCC->PLLCFGR &= ~(0x1F);
+	RCC->PLLCFGR |= 0x08; // PLLM=8
+	
+	// configure PLLN
+	RCC->PLLCFGR &= ~(0x1FF<<6);
+	RCC->PLLCFGR |= 160<<6; // PLLN=160
+	
+	// configure PLLP
+	RCC->PLLCFGR &= ~(0x03<<16);
+	RCC->PLLCFGR |= 0x01<<16;
+	
+	RCC->CR |= 1<<24; // enable PLL
+	while(!(RCC->CR & (1<<25))); // wait for PLL
+	
+	// assign PLL as the system clock
+	RCC->CFGR &= ~(0x03);
+	RCC->CFGR |= 0x02;
+}
+
+void systick_init(void){
+//	// HCLK prescale=1 (AHB max=80MHz)
+//	RCC->CFGR &= ~(0xF<<4);
+//	
+//	// APB1 prescalar=4 (APB1 max=42MHz)
+//	RCC->CFGR &= ~(0x7<<10);
+//	RCC->CFGR |= 0x5<<10;
+//	
+//	// APB2 prescaler=2 (APB2 max = 84MHz)
+//	RCC->CFGR &= ~(0x7<<13);
+//	RCC->CFGR |= 0x4<<13;
 }
