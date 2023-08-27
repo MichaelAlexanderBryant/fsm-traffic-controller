fbb1e1797b91a7db5f3d330262ee0e9dfc67d9cb
MichaelAlexanderBryant <m.alexander.bryant@gmail.com>
2023-08-26 17:13:18 -0700

    Clean code and comments

--- main.c
+++ main.c
@@ -1,25 +1,25 @@
 #include "stm32f4xx.h"
 
+// finite-state machine {6-bit output, time delay, state transitions}
 struct state {
-	uint32_t set_lights; // 6-bit output where left three are for North and right three are for East (green, yellow, red)
+	uint32_t set_lights;
 	uint32_t time_delay;
 	uint32_t next_state[4];
 };
 
-typedef const struct state state_t;
+typedef const struct state state_t; // store state struct in flash ROM
 
 // states
-#define GO_NORTH 0
-#define WAIT_NORTH 1
-#define	GO_EAST 2
-#define WAIT_EAST 3
+#define GO_Y 0
+#define WAIT_Y 1
+#define	GO_X 2
+#define WAIT_X 3
 
-// finite-state machine {6-bit output, time delay, state transitions}
 state_t FSM[4]={
- {0x21,3000,{GO_NORTH,WAIT_NORTH,GO_NORTH,WAIT_NORTH}},
- {0x22, 500,{GO_EAST,GO_EAST,GO_EAST,GO_EAST}},
- {0x0C,3000,{GO_EAST,GO_EAST,WAIT_EAST,WAIT_EAST}},
- {0x14, 500,{GO_NORTH,GO_NORTH,GO_NORTH,GO_NORTH}}
+ {0x21,3000,{GO_Y,WAIT_Y,GO_Y,WAIT_Y}},
+ {0x22, 500,{GO_X,GO_X,GO_X,GO_X}},
+ {0x0C,3000,{GO_X,GO_X,WAIT_X,WAIT_X}},
+ {0x14, 500,{GO_Y,GO_Y,GO_Y,GO_Y}}
 };
 
 uint32_t current_state; // index the current state
@@ -35,7 +35,7 @@ int main(void) {
 	GPIOA->MODER |= 0x00000555; // 010101010101
 	GPIOC->MODER &= 0x0000000F; // PC0-PC1 (input)
 	
-	current_state = GO_NORTH; // initial state
+	current_state = GO_Y; // initial state
 	while(1){
 		GPIOA->ODR = FSM[current_state].set_lights;
 		// wait FSM[current_state].time_delay
